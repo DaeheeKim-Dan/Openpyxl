7f9b47f4de2e3646b96dca69df82e88a98abbf6d
DESKTOP-MGASF53\Daehee <daeheekim7978@gmail.com>
2018-04-17 01:20:15 +0900

    Named Range 리스트를 추출하고 그 주소와 값을 컨트롤 하기

--- o002_NamedCellControl.py
+++ o002_NamedCellControl.py
@@ -5,14 +5,32 @@ def deleteSC(address):
     return address.replace('$','')
 
 
+def get_addr(defName):
+    dn=list(defName.destinations)
+    return deleteSC(dn[0][1])
+
 wb=ox.load_workbook('o002_NamedCellControl.xlsx')
-sht=wb[list(wb.defined_names['test'].destinations)[0][0]]
-address=deleteSC(list(wb.defined_names['test'].destinations)[0][1])
+sht=wb.active
 
-sht[address][0][0].value='MergedCell'
-print(sht[address][0][0].value)
+# Named Range List를 추출하고, 그 주소를 함께 출력
+defNames=wb.defined_names.definedName
+for item in defNames:
+    print(item.name, ' ---> ', item.attr_text)
+    
 
-sht[address][0][0].value='NamedCell'
-print(sht[address][0][0].value)
+# 전체 named range의 값을 named range의 이름으로 변경
+for i in range(len(defNames)):
+    print(get_addr(defNames[i]))
+
+for i in range(len(defNames)):
+    sht[get_addr(defNames[i]).split(':')[0]].value=''
+
+for i in range(len(defNames)):
+    sht[get_addr(defNames[i]).split(':')[0]].value=defNames[i].name
+
+    
+##sht=wb[list(wb.defined_names['test'].destinations)[0][0]]
+##address=deleteSC(list(wb.defined_names['test'].destinations)[0][1])
+
 
 wb.save('o002_NamedCellControl.xlsx')
